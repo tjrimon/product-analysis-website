2947f24633207bd75a5d96cdd1121320ef6106e1
tjrimonm <tjrimonr@gmail.com>
2022-04-04 10:39:19 +0600

    header style done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,20 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import './Header.css'
 
 const Header = () => {
     return (
-        <div>
-            <Link className='' to='/'>Home</Link>
-            <Link to='/reviews'>Reviews</Link>
-            <Link to='/dashboard'>Dashboard</Link>
-            <Link to='/blogs'>Blogs</Link>
-        </div>
+        <div className='flex items-center justify-between border-b border-slate-200 px-10'>
+            <h3 className='text-2xl uppercase '>Packbo</h3>
+            <nav>
+                <ul className='flex justify-center items-center py-5'>
+                    <li className='pb-1 mx-3 text-[18px]  hover:text-orange-400'><NavLink to='/'>Home</NavLink></li>
+                    <li className='pb-1 mx-3 text-[18px]  hover:text-orange-400'><NavLink to='/reviews'>Reviews</NavLink></li>
+                    <li className='pb-1 mx-3 text-[18px]  hover:text-orange-400'><NavLink to='/dashboard'>Dashboard</NavLink></li>
+                    <li className='pb-1 mx-3 text-[18px]  hover:text-orange-400'> <NavLink to='/blogs'>Blogs</NavLink></li>
+                </ul>
+            </nav>
+        </div >
     );
 };
 
